5c8d2f8b6c445b4ef4982981bf0ca69fe97426c5
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-07-31 16:19:18 +0400

    fix: update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,32 @@
+const webpack = require('webpack');
 const path = require('path');
 
-module.exports = (options, webpack) => {
+module.exports = (options) => {
   const lazyImports = [
     '@nestjs/microservices/microservices-module',
     '@nestjs/websockets/socket-module',
   ];
 
+  const { plugins, ...config } = options;
+
   return {
-    ...options,
-    entry: './src/main.ts',
-    target: 'node',
+    ...config,
+    entry: ['./src/lambda.ts'],
     externals: [],
-    mode: 'production',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
       libraryTarget: 'commonjs2',
     },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
     plugins: [
-      ...options.plugins,
+      ...plugins,
       new webpack.IgnorePlugin({
         checkResource(resource) {
           if (lazyImports.includes(resource)) {
             try {
-              require.resolve(resource);
+              require.resolve(resource, {
+                paths: [process.cwd()],
+              });
             } catch (err) {
               return true;
             }
